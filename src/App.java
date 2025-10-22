469e6fca8b5548c892d451f68f7d3e1bed336f56
Curro Portillo <211curroportillo@gmail.com>
2025-10-22 19:45:52 +0200

    ejercicio 6

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int i = 320;
+        do{
+            System.out.print(i + " ");
+            i-=20;
+        }while(i>=160);
     }
 }
